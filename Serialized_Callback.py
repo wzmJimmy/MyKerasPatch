ba00c6acebda9210cf92896693be8ae45a808617
Jimmywzm <zwang2236@wisc.edu>
2018-11-22 17:24:42 -0600

    bug fix3

--- Serialized_Callback.py
+++ Serialized_Callback.py
@@ -6,7 +6,7 @@ import keras.backend as K
 'Picklable' version of Keras official ReduceLROnPlateau 
 Simply change original value of #self.monitor_op from the
 local lambda to class function.
-Moreover, a @reset option (default True) is added for decide
+Moreover, a @reset option (default True) is added to decide
 resume or not.
 '''
 class ReduceLROnPlateau_picklable(keras.callbacks.Callback):
@@ -101,87 +101,88 @@ class ReduceLROnPlateau_picklable(keras.callbacks.Callback):
     def in_cooldown(self):
         return self.cooldown_counter > 0
 
-class ReduceLROnPlateau_picklable(Callback):
-    def __init__(self, monitor='val_loss', factor=0.1, patience=10,reset = True,
-                 verbose=0, mode='auto', min_delta=1e-4, cooldown=0, min_lr=0,
-                 **kwargs):
-        super(ReduceLROnPlateau, self).__init__()
+''' 
+'Picklable' version of Keras official ReduceLROnPlateau 
+Simply add a @reset option (default True) for deciding
+resume or not.
+'''
+class EarlyStopping_picklable(Callback):
+    def __init__(self, monitor='val_loss',min_delta=0, patience=0, verbose=0,
+                 reset = True,mode='auto',baseline=None,restore_best_weights=False):
+        super(EarlyStopping, self).__init__()
 
         self.monitor = monitor
-        if factor >= 1.0:
-            raise ValueError('ReduceLROnPlateau '
-                             'does not support a factor >= 1.0.')
-        if 'epsilon' in kwargs:
-            min_delta = kwargs.pop('epsilon')
-            warnings.warn('`epsilon` argument is deprecated and '
-                          'will be removed, use `min_delta` instead.')
-        self.factor = factor
-        self.min_lr = min_lr
-        self.min_delta = min_delta
+        self.baseline = baseline
         self.patience = patience
         self.verbose = verbose
-        self.cooldown = cooldown
-        self.cooldown_counter = 0  # Cooldown counter.
+        self.min_delta = min_delta
         self.wait = 0
-        self.best = 0
-        self.mode = mode
-        self.monitor_op = None
-        self.reset = reset
-        self._reset()
+        self.stopped_epoch = 0
+        self.restore_best_weights = restore_best_weights
+        self.best_weights = None
+        self.reset = reset 
 
-    def _reset(self):
-        """Resets wait counter and cooldown counter.
-        """
-        if self.mode not in ['auto', 'min', 'max']:
-            warnings.warn('Learning Rate Plateau Reducing mode %s is unknown, '
-                          'fallback to auto mode.' % (self.mode),
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('EarlyStopping mode %s is unknown, '
+                          'fallback to auto mode.' % mode,
                           RuntimeWarning)
-            self.mode = 'auto'
-        if (self.mode == 'min' or
-           (self.mode == 'auto' and 'acc' not in self.monitor)):
-            self.monitor_op = lambda a, b: np.less(a, b - self.min_delta)
-            self.best = np.Inf
+            mode = 'auto'
+
+        if mode == 'min':
+            self.monitor_op = np.less
+        elif mode == 'max':
+            self.monitor_op = np.greater
         else:
-            self.monitor_op = lambda a, b: np.greater(a, b + self.min_delta)
-            self.best = -np.Inf
-        self.cooldown_counter = 0
-        self.wait = 0
+            if 'acc' in self.monitor:
+                self.monitor_op = np.greater
+            else:
+                self.monitor_op = np.less
+
+        if self.monitor_op == np.greater:
+            self.min_delta *= 1
+        else:
+            self.min_delta *= -1
 
     def on_train_begin(self, logs=None):
-        if self.reset: self._reset()
+        if self.reset:
+            self.wait = 0
+            self.stopped_epoch = 0
+            if self.baseline is not None:
+                self.best = self.baseline
+            else:
+                self.best = np.Inf if self.monitor_op == np.less else -np.Inf
 
     def on_epoch_end(self, epoch, logs=None):
-        logs = logs or {}
-        logs['lr'] = K.get_value(self.model.optimizer.lr)
-        current = logs.get(self.monitor)
+        current = self.get_monitor_value(logs)
         if current is None:
+            return
+
+        if self.monitor_op(current - self.min_delta, self.best):
+            self.best = current
+            self.wait = 0
+            if self.restore_best_weights:
+                self.best_weights = self.model.get_weights()
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.stopped_epoch = epoch
+                self.model.stop_training = True
+                if self.restore_best_weights:
+                    if self.verbose > 0:
+                        print('Restoring model weights from the end of '
+                              'the best epoch')
+                    self.model.set_weights(self.best_weights)
+
+    def on_train_end(self, logs=None):
+        if self.stopped_epoch > 0 and self.verbose > 0:
+            print('Epoch %05d: early stopping' % (self.stopped_epoch + 1))
+
+    def get_monitor_value(self, logs):
+        monitor_value = logs.get(self.monitor)
+        if monitor_value is None:
             warnings.warn(
-                'Reduce LR on plateau conditioned on metric `%s` '
+                'Early stopping conditioned on metric `%s` '
                 'which is not available. Available metrics are: %s' %
                 (self.monitor, ','.join(list(logs.keys()))), RuntimeWarning
             )
-
-        else:
-            if self.in_cooldown():
-                self.cooldown_counter -= 1
-                self.wait = 0
-
-            if self.monitor_op(current, self.best):
-                self.best = current
-                self.wait = 0
-            elif not self.in_cooldown():
-                self.wait += 1
-                if self.wait >= self.patience:
-                    old_lr = float(K.get_value(self.model.optimizer.lr))
-                    if old_lr > self.min_lr:
-                        new_lr = old_lr * self.factor
-                        new_lr = max(new_lr, self.min_lr)
-                        K.set_value(self.model.optimizer.lr, new_lr)
-                        if self.verbose > 0:
-                            print('\nEpoch %05d: ReduceLROnPlateau reducing '
-                                  'learning rate to %s.' % (epoch + 1, new_lr))
-                        self.cooldown_counter = self.cooldown
-                        self.wait = 0
-
-    def in_cooldown(self):
-        return self.cooldown_counter > 0
+        return monitor_value
